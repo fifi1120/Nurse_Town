57df1c29d148ddccfc298196eb46f22e60ce3257
Burned357Waffles <bwatanabe@sfsu.edu>
2025-03-13 16:06:47 -0700

    Main menu created
    Added main menu placeholder. can fill out details and tweak visuals later. canvas and scene transitions functional

---
+++ Assets/Scripts/UI/Menu/LoadingManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UI.Menu
+{
+
+    public class LoadingManager : MonoBehaviour
+    {
+        public static LoadingManager Instance;
+        [SerializeField] private Canvas _loadingScreen;
+
+        public void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+                DontDestroyOnLoad(gameObject);
+            }
+
+            _loadingScreen = GameObject.Find("Loading Screen").GetComponent<Canvas>();
+        }
+
+        public void LoadScene(string sceneName)
+        {
+            _loadingScreen.enabled = true;
+            StartCoroutine(LoadingScreen(sceneName));
+        }
+
+        public IEnumerator LoadingScreen(string sceneName)
+        {
+            yield return new WaitForSeconds(2);
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Menu/StartGame.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI.Menu
+{
+    public class StartGame
+    {
+        [SerializeField] private GameObject mainMenu;
+        [SerializeField] private GameObject startGame;
+        [SerializeField] private Button backButton;
+
+        private Canvas _mainMenuCanvas;
+        private Canvas _creditsCanvas;
+        private Canvas _backCanvas;
+        
+        void Start()
+        {
+            _mainMenuCanvas = mainMenu.GetComponent<Canvas>();
+            _creditsCanvas = startGame.GetComponent<Canvas>();
+        }
+        
+        public void Back()
+        {
+            Menu.CanvasTransition(_creditsCanvas, _mainMenuCanvas);
+        }
+        
+    }
+}
