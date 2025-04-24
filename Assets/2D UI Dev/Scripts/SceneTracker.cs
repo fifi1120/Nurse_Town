498b935d91c63a8ae3afcad06c97ab7e20e268b8
Burned357Waffles <bwatanabe@sfsu.edu>
2025-04-23 23:15:41 -0700

    fixed scene linking

--- Assets/2D UI Dev/Scripts/SceneTracker.cs
+++ Assets/2D UI Dev/Scripts/SceneTracker.cs
@@ -26,11 +26,11 @@ public class SceneTracker : MonoBehaviour
     }
 
     // Separate functions for each scene (to be assigned in Unity Inspector)
-    public void LoadScene1() { MarkSceneAsVisited("Scenes/InterviewScene", 1); }
-    public void LoadScene2() { MarkSceneAsVisited("Scenes/InterviewScene", 2); }
-    public void LoadScene3() { MarkSceneAsVisited("Scenes/InterviewScene", 3); }
-    public void LoadScene4() { MarkSceneAsVisited("Scenes/InterviewScene", 4); }
-    public void LoadScene5() { MarkSceneAsVisited("Scenes/InterviewScene", 5); }
+    public void LoadScene1() { MarkSceneAsVisited("Scenes/ICU_Interview/Primary Nurse", 1); }
+    public void LoadScene2() { MarkSceneAsVisited("Scenes/ICU_Interview/ICU Anesthesiologist", 2); }
+    public void LoadScene3() { MarkSceneAsVisited("Scenes/ICU_Interview/MedicalStudent", 3); }
+    public void LoadScene4() { MarkSceneAsVisited("Scenes/ICU_Interview/ICU Nurse", 4); }
+    public void LoadScene5() { MarkSceneAsVisited("Scenes/ICU_Interview/ICU Doctor", 5); }
 
     private void MarkSceneAsVisited(string sceneName, int sceneNumber)
     {
